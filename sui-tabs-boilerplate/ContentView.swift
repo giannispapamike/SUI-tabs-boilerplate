a43c073bed7de1ce67e407dcf5cc64a03039a1e1
Giannis Papamichail <papamichail.giannis@gmail.com>
2024-06-22 16:34:53 +0300

    Setup xcode project

---
+++ sui-tabs-boilerplate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sui-tabs-boilerplate
+//
+//  Created by Ioannis Papamichail on 22/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sui-tabs-boilerplate/sui_tabs_boilerplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  sui_tabs_boilerplateApp.swift
+//  sui-tabs-boilerplate
+//
+//  Created by Ioannis Papamichail on 22/6/24.
+//
+
+import SwiftUI
+
+@main
+struct sui_tabs_boilerplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
